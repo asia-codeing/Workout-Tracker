617392f20c1ad743ef4de88c59131924d56b5eaf
Asia Alnahi <asia.alius@gmail.com>
2021-06-05 14:13:49 -0700

    replace find all with aggregate

--- routes/api.js
+++ routes/api.js
@@ -11,17 +11,6 @@ router.post("/api/workouts", (req, res) => {
     });
 });
 
-
-// router.get("/api/workouts", (req, res) => {
-//     Workout.find({})
-//     .then(dbWorkout => {
-//         res.json(dbWorkout);
-//     })
-//     .catch(err => {
-//         res.status(400).json(err);
-//     });
-// });
-
 router.get("/api/workouts/range", (req, res) => {
     Workout.aggregate([
         { 
