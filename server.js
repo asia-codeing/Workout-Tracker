c3db5deaebc4b9c66d3c782030341de90b11cb59
Asia Alnahi <asia.alius@gmail.com>
2021-06-04 20:00:32 -0700

    deploying

--- server.js
+++ server.js
@@ -12,13 +12,22 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB"
-,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workoutDB',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+);
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB"
+// ,{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// });
 
 //Routes
 app.use(require("./routes/api.js"));
